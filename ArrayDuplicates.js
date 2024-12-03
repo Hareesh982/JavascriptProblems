aac02ebf91d792384174485b6c31183273a06d76
D Hareesh <hareeshduddupudi9271@gmail.com>
2024-12-03 18:42:45 +0530

    Array duplicates modified

--- ArrayDuplicates.js
+++ ArrayDuplicates.js
@@ -1,7 +1,38 @@
 let arr = [1,2,2,3,3,3,4,4,4,4,5,6,7,7,8,9]
 n = arr.length
 
-// identifying the duplicates in an array
+// frequency of an array
+
+frequency = {}
+for(let i of arr){
+    if(frequency[i] == undefined){
+        frequency[i] = 1
+    }
+    else{
+        frequency[i]++
+    }
+}
+console.log("Frequency of an array : ",frequency);
+
+// finding the duplicates in an array using frequency
+
+arr_dup_freq = []
+for(let key of Object.keys(frequency)){
+    if(frequency[key]>1){
+        arr_dup_freq.push(parseInt(key))
+    }
+}
+console.log("Array duplicates using frequency : ",arr_dup_freq);
+
+// finding the unique elements in an array using frequency
+
+arr_unq_freq = []
+for(let key of Object.keys(frequency)){
+    arr_unq_freq.push(parseInt(key))
+}
+console.log("Unique elements using frequency : ",arr_unq_freq);
+
+// finding the duplicates in an array
 
 let duplicates = []
 let value
@@ -14,21 +45,7 @@ for(let i=0;i<n;i++){
         duplicates.push(value)
     }
 }
-console.log(duplicates);
-console.log(arr);
-
-// frequency of an array
-
-frequency = {}
-for(let i of arr){
-    if(frequency[i] == undefined){
-        frequency[i] = 1
-    }
-    else{
-        frequency[i]++
-    }
-}
-console.log(frequency);
+console.log("Finding duplicates : ",duplicates);
 
 // removing duplicates from an array
 
@@ -37,6 +54,6 @@ for(let i=0;i<arr.length;i++){
         arr.splice(arr.lastIndexOf(arr[i]),1)
     }
 }
-console.log(arr);
+console.log("Removing duplicates : ",arr);
 
 
